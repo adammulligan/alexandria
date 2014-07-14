8edd3665b887b7115a7185c30f2f6fc4bb74524a
Adam Mulligan <adammulligan@gmail.com>
2014-07-14 21:07:33 +0100

    Load aws config in carrierwave init

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,10 +1,10 @@
-require 'yaml'
+AWS_CONFIG = YAML.load_file(Rails.root.join('config/secrets.yml'))[Rails.env]
 
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => Rails.application.secrets.aws_access_key_id,
-    :aws_secret_access_key  => Rails.application.secrets.aws_secret_access_key,
+    :aws_access_key_id      => AWS_CONFIG["aws_access_key_id"],
+    :aws_secret_access_key  => AWS_CONFIG["aws_secret_access_key"],
     :region                 => 'eu-west-1'
   }
 
