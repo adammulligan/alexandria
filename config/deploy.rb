8e09d32b7859cc3a8daa4827b15a408cd13a773e
Adam Mulligan <adammulligan@fastmail.fm>
2014-08-23 18:29:11 +0100

    Update configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'https://github.com/adammulligan/alexandria.git'
 
 set :branch, "master"
 
-#set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/aws.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
